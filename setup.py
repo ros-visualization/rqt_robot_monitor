581e1766164b5d0a132a9aef08ec9637234a1e57
Jacob Hassold <jhassold@dcscorp.com>
2020-04-06 09:09:47 -0400

    Get Colcon to "compile" package Distro A, OPSEC #2893
    Signed-off-by: Jacob Hassold <jhassold@dcscorp.com>

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ setup(
     packages = [package_name],
     package_dir = {'': 'src'},
     data_files = [
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        # ('share/ament_index/resource_index/packages',
+        #     ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name, ['plugin.xml']),
         ('share/' + package_name, ['bag_plugin.xml']),
