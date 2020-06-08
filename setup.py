397ff7a2ecf102027c7764cb182dbd2d76a2828d
Arne Hitzmann <arne.hitzmann@gmail.com>
2020-06-08 13:07:58 +0900

    removed bag_plugin export and rqt_bag dependency

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name, ['plugin.xml']),
-        ('share/' + package_name, ['bag_plugin.xml']),
         ('share/' + package_name + '/resource',
             ['resource/robotmonitor_mainwidget.ui']),
         ('share/' + package_name + '/resource',
