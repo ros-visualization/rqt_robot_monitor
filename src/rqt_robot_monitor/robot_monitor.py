443ea1090aa3753d1db48d019fc44369d3f78cc7
alemme <17647526+alemme@users.noreply.github.com>
2020-09-18 19:41:08 +0200

    view all devices with correct status

--- src/rqt_robot_monitor/robot_monitor.py
+++ src/rqt_robot_monitor/robot_monitor.py
@@ -158,21 +158,32 @@ class RobotMonitorWidget(QWidget):
         for name, status in status.items():
             # Compute path and walk to appropriate subtree
             path = name.split('/')
+            tmp_tree = self._tree
+            tmp_err_tree = self._err_tree
+            tmp_warn_tree = self._warn_tree
             if path[0] == '':
                 path = path[1:]
-            tmp_tree = self._tree
+                tmp_tree = tmp_tree['Robot']
+                tmp_tree.update(status, 'Robot')
+
+            leaf = path[-1]
             for p in path:
-                tmp_tree = tmp_tree[p]
-            tmp_tree.update(status, util.get_resource_name(name))
+                if p != '':
+                    tmp_tree = tmp_tree[p]
+                    tmp_err_tree = tmp_err_tree[p]
+                    tmp_warn_tree = tmp_warn_tree[p]
+                    if p == leaf:
+                        tmp_tree.update(status, p)
+                        # Check for warnings
+                        if status.level == DiagnosticStatus.WARN:
+                            tmp_warn_tree[name].update(status, p)
 
-            # Check for warnings
-            if status.level == DiagnosticStatus.WARN:
-                self._warn_tree[name].update(status, name)
+                        # Check for errors
+                        if (status.level == DiagnosticStatus.ERROR or
+                            status.level == DiagnosticStatus.STALE):
+                            tmp_err_tree.update(status, p)
 
-            # Check for errors
-            if (status.level == DiagnosticStatus.ERROR or
-                status.level == DiagnosticStatus.STALE):
-                self._err_tree[name].update(status, name)
+
 
         # For any items in the tree that were not updated, remove them
         self._tree.prune()
